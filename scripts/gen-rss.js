e77ebef0e5e4bf7e01de24db5fd47038456e58e0
doniwirawan <doniwirawan166@gmail.com>
2022-05-02 13:47:02 +0800

    adding mine

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Doni WIrawan',
+    site_url: 'https://doniwirawan.com',
+    feed_url: 'https://doniwirawan.com/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
