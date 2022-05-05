939175c1bc3666d98136272f7af64c22608f44b1
doniwirawan <doniwirawan166@gmail.com>
2022-05-05 10:13:55 +0800

    delete pwa

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 const withNextra = require('nextra')
     ('nextra-theme-blog', './theme.config.js')
-const withPWA = require("next-pwa");
+// const withPWA = require("next-pwa");
 
 module.exports = withNextra()
 
-module.exports = withPWA({
-    pwa: {
-        dest: "public",
-        register: true,
-        skipWaiting: true,
-        disable: process.env.NODE_ENV === "development",
-    },
-});
+// module.exports = withPWA({
+//     pwa: {
+//         dest: "public",
+//         register: true,
+//         skipWaiting: true,
+//     },
+// });
