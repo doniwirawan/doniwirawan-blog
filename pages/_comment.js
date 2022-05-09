c4c0faeb2473a2fb79db9d38d5da233e2de8c63d
“doniwirawan” <doniwirawan166@gmail.com>
2022-05-09 20:51:55 +0800

    giscus work

--- pages/_comment.js
+++ pages/_comment.js
@@ -1,4 +1,6 @@
 import Giscus from "@giscus/react";
+import { useRouter } from 'next/router'
+
 
 const Comment = () => {
     const projectRepo = process.env.REACT_APP_GISCUS_PROJECT_REPO;
@@ -8,24 +10,35 @@ const Comment = () => {
     const discussionCategoryName =
         process.env.REACT_APP_GISCUS_DISCUSSION_CATEGORY_NAME;
 
-    return (
-        <>
-            <Giscus
-                id="comment-id"
-                repo={projectRepo}
-                repoId={projectRepoId}
-                category={discussionCategoryName}
-                categoryId={discussionCategoryId}
-                mapping="pathname"
-                reactionsEnabled="0"
-                emitMetadata="0"
-                inputPosition="top"
-                theme="dark"
-                lang="en"
-                loading="lazy"
-            />
-        </>
-    );
+    const router = useRouter()
+    if (router.pathname === '/' && router.pathname === '/posts') {
+        return (
+            <>
+                <h2>Tidak jalan</h2>
+                {console.log(router.pathname)}
+            </>
+        );
+    } else {
+        return (
+            <>
+                <Giscus
+                    id="comment-id"
+                    repo="doniwirawan/doniwirawan-blog"
+                    repoId="R_kgDOHRIlkw"
+                    category="General"
+                    categoryId="DIC_kwDOHRIlk84CPAde"
+                    mapping="pathname"
+                    reactionsEnabled="0"
+                    emitMetadata="0"
+                    inputPosition="top"
+                    theme="dark"
+                    lang="en"
+                    loading="lazy"
+                />
+            </>
+        );
+    }
+
 };
 
 export default Comment;
